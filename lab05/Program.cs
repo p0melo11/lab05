de1ee028b0b1c17688c6044a555aca2b8f7a3b0d
p0melo11 <dimabob463@gmail.com>
2022-10-22 23:24:36 +0300

    Добавьте файлы проекта.

---
+++ lab05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
